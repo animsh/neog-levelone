597c5e3da52c31209fd94daeba18b57012c120dc
Sagar More <animsh.more@gmail.com>
2023-07-04 13:14:18 +0530

    feat: ex13

---
+++ ex13.js
@@ -0,0 +1,14 @@
+// hint: there's an error in these objects
+// if you copy/paste blindly it won't work
+let questionOne = {
+  question: "Who is my favorite superhero?",
+  answer: "Dhruv",
+};
+
+let questionTwo = {
+  question: "Which is my favorite sad song?",
+  answer: "Channa Meraya",
+};
+
+let questions = [questionOne, questionTwo];
+console.log(questions);
